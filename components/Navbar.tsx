95cd31cfa01c229e952103e93b1c63975c30569b
gilary bacnis <gilarybacnis@gmail.com>
2023-10-23 18:08:16 +0900

    added event listener

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,23 +2,28 @@
 import React from 'react';
 import { useCallback , useState } from 'react';
 import NavbarItem from "./NavbarItem";
-import { BsChevronDown,  BsSearch, BsBell } from 'react-icons/bs';
+import { BsChevronDown } from 'react-icons/bs';
 import MobileMenu from './MobileMenu';
 import AccountMenu from './AccountMenu';
 
 export default function Navbar(){
   const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
+
   const toggleMobileMenu = useCallback(() =>{
     setShowMobileMenu((current) => (!current));
   }, []);
 
+  const toggleAccountMenu = useCallback(() =>{
+      setShowAccountMenu((current) => (!current));
+    }, []);
 
   return (
     <nav className="w-full fixed z-40 px-4 lg:px-8">
       <div className="
       px-4
       md: px-16
-      py-6
+      py-4
       flex
       flex-row
       items-center
@@ -28,7 +33,7 @@ export default function Navbar(){
       bg-opacity-90
       ">
         <img className="h-4 lg:h-6" src="/images/logo.png" alt="Logo"/>
-        <div className="flex flex-row gap-7 ml-8 hidden lg:flex
+        <div className="flex flex-row gap-5 ml-8 hidden lg:flex
         ">
           <NavbarItem label="Home" />
           <NavbarItem label="TV Shows" />
@@ -37,21 +42,22 @@ export default function Navbar(){
           <NavbarItem label="My List" />
           <NavbarItem label="Browse by Languages" />
         </div>
-        <div onClick={toggleMobileMenu}className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
+        <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
           <p className="text-white text-sm ">Browse</p>
-          <BsChevronDown className="text-white transition"/>
+          <BsChevronDown className={`text-white transition ${showMobileMenu ? 'rotate-180' : 'rotate-0' }`}/>
           <MobileMenu visible={showMobileMenu} />
         </div>
-        <div className="flex flex-row ml-auto gap-7 items-center">
+        <div className="flex flex-row ml-auto gap-5 items-center">
           <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
-          <BsSearch className=""/>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="search-icon ltr-4z3qvp e1svuwfo1" data-name="MagnifyingGlass" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10ZM15.6177 17.0319C14.078 18.2635 12.125 19 10 19C5.02944 19 1 14.9706 1 10C1 5.02944 5.02944 1 10 1C14.9706 1 19 5.02944 19 10C19 12.125 18.2635 14.078 17.0319 15.6177L22.7071 21.2929L21.2929 22.7071L15.6177 17.0319Z" fill="currentColor"></path></svg>
           </div>
           <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
-          <BsBell className=""/>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="ltr-4z3qvp e1svuwfo1" data-name="Bell" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M13.0002 4.07092C16.3924 4.55624 19 7.4736 19 11V15.2538C20.0489 15.3307 21.0851 15.4245 22.1072 15.5347L21.8928 17.5232C18.7222 17.1813 15.4092 17 12 17C8.59081 17 5.27788 17.1813 2.10723 17.5232L1.89282 15.5347C2.91498 15.4245 3.95119 15.3307 5.00003 15.2538V11C5.00003 7.47345 7.60784 4.55599 11.0002 4.07086V2H13.0002V4.07092ZM17 15.1287V11C17 8.23858 14.7614 6 12 6C9.2386 6 7.00003 8.23858 7.00003 11V15.1287C8.64066 15.0437 10.3091 15 12 15C13.691 15 15.3594 15.0437 17 15.1287ZM8.62593 19.3712C8.66235 20.5173 10.1512 22 11.9996 22C13.848 22 15.3368 20.5173 15.3732 19.3712C15.3803 19.1489 15.1758 19 14.9533 19H9.0458C8.82333 19 8.61886 19.1489 8.62593 19.3712Z" fill="currentColor"></path></svg>
           </div>
-          <div className="flex flex-row items-center gap-2 cursor-pointer relative">
-            <img className="w-8 h-8 lg:h-10 rounded-md overflow-hidden" src="/images/default-blue.png" alt="icon"/>
-            <AccountMenu visible />
+          <div onClick={toggleAccountMenu} className="flex flex-row items-center gap-2 cursor-pointer relative">
+            <img className="w-8 h-8 lg:h-8 rounded-md overflow-hidden" src="/images/default-blue.png" alt="icon"/>
+            <BsChevronDown className={`text-white transition ${showAccountMenu ? 'rotate-180' : 'rotate-0' }`}/>
+            <AccountMenu visible={showAccountMenu}/>
           </div>
         </div>
       </div>
